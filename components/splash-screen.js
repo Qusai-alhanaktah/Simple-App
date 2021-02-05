9a0d836a742fbd71308fd8547f22c0bd4ab6aa2c
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2021-02-06 01:55:16 +0200

    Add sign up/in view

--- components/splash-screen.js
+++ components/splash-screen.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button, Modal, TextInput } from 'react-native';
 
 const Home = (props) => {
+  const [showSignUpForm, setShowSignUpForm] = useState(false)
+  const [showSignInForm, setShowSignInForm] = useState(false)
+
+  const signIn = () => {
+
+  }
+
+  const signUp = () => {
+
+  }
+
   return(
     <View style={styles.container}>
       <View style={styles.header}>
@@ -9,8 +20,40 @@ const Home = (props) => {
       </View>
       <View style={styles.starting}>
         <Text style={styles.text}>Welcome To My App</Text>
-        <Button title="Get Started" color="gray" />
+        <Button title="Get Started" color="gray" onPress = { ()=> setShowSignUpForm(true) }/>
       </View>
+      <Modal visible={showSignUpForm} style={styles.modal}>
+        <Button onPress={()=> setShowSignUpForm(false)} title="X" color='red'/>
+        <Text>Sign Up</Text>
+        <View>
+          <Text>UserName:</Text>
+          <TextInput placeholder='Type Your Username' style={styles.modalText} onChangeText={(value)=> setUser({...user, 'username': value})}/>
+        </View>
+        <View>
+          <Text>Password:</Text>
+          <TextInput placeholder='Type Your Password' style={styles.modalText} onChangeText={(value)=> setUser({...user, 'password': value})}/>
+        </View>
+        <View>
+          <Text>Email:</Text>
+          <TextInput placeholder='Type Your Email' style={styles.modalText} onChangeText={(value)=> setUser({...user, 'email': value})}/>
+        </View>
+        <Button onPress={()=> {setShowSignUpForm(false); signUp()}} title="Submit" color='#eeff41'/>
+        <Text onPress={ ()=> { setShowSignUpForm(false); setShowSignInForm(true) }}>Sign In</Text>
+      </Modal>
+      <Modal visible={showSignInForm} style={styles.modal}>
+        <Button onPress={()=> setShowSignInForm(false)} title="X" color='red'/>
+        <Text>Sign In</Text>
+        <View>
+          <Text>UserName:</Text>
+          <TextInput placeholder='Type Your Username' style={styles.modalText} onChangeText={(value)=> setUser({...user, 'username': value})}/>
+        </View>
+        <View>
+          <Text>Password:</Text>
+          <TextInput placeholder='Type Your Password' style={styles.modalText} onChangeText={(value)=> setUser({...user, 'password': value})}/>
+        </View>
+        <Button onPress={()=> {setShowSignInForm(false); signIn()}} title="Submit" color='#eeff41'/>
+        <Text onPress={ ()=> { setShowSignInForm(false); setShowSignUpForm(true) }}>Sign Up</Text>
+      </Modal>
     </View>
   );
 }
@@ -19,6 +62,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#00D068'
+  },
+  modal: {
+
   },
 
   header: {
